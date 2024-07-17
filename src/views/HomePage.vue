712bd0ea5cc28f7d11086f9675a98fc52bc4a4a9
Julia Wikdahl <julia_2000@live.se>
2024-07-17 14:36:00 +0200

    fixat så jag får ut rätt information om flygsökningen

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -48,18 +48,46 @@
         </div>
       </form>
     </div>
-    <div v-if="searchResults" class="search-results">
-      <h3>Sökresultat</h3>
-      <ul>
-        <li v-for="result in searchResults" :key="result.id">
-          {{ result.offerItems[0].services[0].segments[0].flightSegment.departure.iataCode }} - {{ result.offerItems[0].services[0].segments[0].flightSegment.arrival.iataCode }}:
-          {{ result.offerItems[0].price.total }} EUR
-        </li>
-      </ul>
-    </div>
-    <div v-if="errorFetchingFlights" class="error-message">
-      Det uppstod ett fel vid hämtning av flygerbjudanden. Vänligen försök igen senare.
-    </div>
+    <div class="search-results">
+  <h3>Sökresultat</h3>
+  <ul>
+    <li v-for="(result, index) in searchResults" :key="index">
+      <div v-if="result.itineraries.length > 0 && result.itineraries[0].segments.length > 0">
+        <p><strong>Flygnummer:</strong> {{ result.itineraries[0].segments[0].carrierCode }}{{ result.itineraries[0].segments[0].number }}</p>
+        <p><strong>Från:</strong> {{ result.itineraries[0].segments[0].departure.iataCode }}, {{ fromInput }}</p>
+        <p><strong>Avresedatum och tid:</strong> {{ formatDate(result.itineraries[0].segments[0].departure.at) }}</p>
+        <p><strong>Till:</strong> {{ result.itineraries[0].segments[result.itineraries[0].segments.length - 1].arrival.iataCode }}, {{ destinationInput }}</p>
+        <p><strong>Ankomstdatum och tid:</strong> {{ formatDate(result.itineraries[0].segments[result.itineraries[0].segments.length - 1].arrival.at) }}</p>
+        <p><strong>Restid:</strong> {{ result.itineraries[0].duration }}</p>
+        <p><strong>Direktflyg:</strong> {{ result.itineraries[0].segments.length === 1 ? 'Ja' : 'Nej' }}</p>
+        
+        <!-- Visa väntetiden -->
+        <p v-if="result.itineraries[0].segments.length > 1">
+          <strong>Väntetid till nästa flyg:</strong> {{ calculateWaitTime(result.itineraries[0].segments[0].arrival.at, result.itineraries[0].segments[1].departure.at) }} minuter
+          
+        </p>
+        
+        <ul v-if="result.itineraries[0].segments.length > 1">
+          <li v-for="(segment, sIndex) in result.itineraries[0].segments.slice(1)" :key="sIndex">
+            <p><strong>Mellanlandning {{ sIndex + 1 }}:</strong></p>
+            <p><strong>Från:</strong> {{ segment.departure.iataCode }}</p>
+            <p><strong>Avresa:</strong> {{ formatDate(segment.departure.at) }}</p>
+            <p><strong>Till:</strong> {{ segment.arrival.iataCode }}</p>
+            <p><strong>Ankomst:</strong> {{ formatDate(segment.arrival.at) }}</p>
+            <p><strong>Flygtid:</strong> {{ segment.duration }}</p>
+          </li>
+        </ul>
+        
+        <p><strong>Antal vuxna:</strong> {{ getNumberOfAdults(result.travelerPricings) }}</p>
+        <p><strong>Antal barn:</strong> {{ getNumberOfChildren(result.travelerPricings) }}</p>
+        <p><strong>Pris:</strong> {{ result.price.total }} {{ result.price.currency }}</p>
+      </div>
+    </li>
+  </ul>
+  <p v-if="errorFetchingFlights" class="error-message">Det uppstod ett fel vid hämtning av flygerbjudanden.</p>
+  <p v-if="searchResults.length === 0 && !errorFetchingFlights">Inga flygerbjudanden hittades.</p>
+</div>
+
   </div>
 </template>
 
@@ -77,12 +105,10 @@ export default {
       returnDate: '',
       adults: 1,
       children: 0,
-      locations: [],
       filteredLocations: [],
       filteredDestinations: [],
-      searchResults: null,
+      searchResults: [],
       accessToken: '',
-      errorFetchingLocations: false,
       errorFetchingFlights: false
     };
   },
@@ -110,16 +136,15 @@ export default {
 
         const data = await response.json();
         this.accessToken = data.access_token;
-        console.log('Access Token:', this.accessToken);
       } catch (error) {
         console.error('Fel vid hämtning av åtkomsttoken:', error);
-        this.errorFetchingLocations = true;
+        this.errorFetchingFlights = true;
       }
     },
-    async fetchLocations() {
+    async fetchLocations(keyword) {
       try {
-        if (this.fromInput) {
-          const response = await fetch(`https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT,CITY&keyword=${this.fromInput}`, {
+        if (keyword) {
+          const response = await fetch(`https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT,CITY&keyword=${keyword}`, {
             headers: {
               'Authorization': `Bearer ${this.accessToken}`
             }
@@ -132,50 +157,17 @@ export default {
           const responseData = await response.json();
 
           if (responseData.data && Array.isArray(responseData.data)) {
-            this.locations = responseData.data;
-            this.filteredLocations = this.locations;
+            return responseData.data;
           } else {
-            console.error('Response data is not an array:', responseData);
             throw new Error('Response data is not an array');
           }
         } else {
-          this.locations = [];
-          this.filteredLocations = [];
+          return [];
         }
       } catch (error) {
         console.error('Fel vid hämtning av platser:', error);
-        this.errorFetchingLocations = true;
-      }
-    },
-    async fetchDestinations() {
-      try {
-        if (this.destinationInput) {
-          const response = await fetch(`https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT,CITY&keyword=${this.destinationInput}`, {
-            headers: {
-              'Authorization': `Bearer ${this.accessToken}`
-            }
-          });
-
-          if (!response.ok) {
-            throw new Error('Misslyckades med att hämta destinationer');
-          }
-
-          const responseData = await response.json();
-
-          if (responseData.data && Array.isArray(responseData.data)) {
-            this.destinations = responseData.data;
-            this.filteredDestinations = this.destinations;
-          } else {
-            console.error('Response data is not an array:', responseData);
-            throw new Error('Response data is not an array');
-          }
-        } else {
-          this.destinations = [];
-          this.filteredDestinations = [];
-        }
-      } catch (error) {
-        console.error('Fel vid hämtning av destinationer:', error);
-        this.errorFetchingDestinations = true;
+        this.errorFetchingFlights = true;
+        return [];
       }
     },
     async submitSearch() {
@@ -187,13 +179,20 @@ export default {
             'Authorization': `Bearer ${this.accessToken}`
           },
           body: JSON.stringify({
-            originLocationCode: this.from,
-            destinationLocationCode: this.destination,
-            departureDate: this.departureDate,
-            returnDate: this.returnDate,
-            adults: this.adults,
-            children: this.children,
-            max: 10
+            originDestinations: [{
+              id: '1',
+              originLocationCode: this.from,
+              destinationLocationCode: this.destination,
+              departureDateTimeRange: {
+                date: this.departureDate,
+                time: '10:00:00' // Just an example time, adjust as needed
+              }
+            }],
+            travelers: this.generateTravelersArray(),
+            sources: ['GDS'],
+            searchCriteria: {
+              maxFlightOffers: 10 // Adjust to the desired number of flight offers
+            }
           })
         });
 
@@ -203,16 +202,17 @@ export default {
 
         const data = await response.json();
         this.searchResults = data.data;
+        console.log('sökresultat', this.searchResults)
       } catch (error) {
         console.error('Fel vid sökning efter flygerbjudanden:', error);
         this.errorFetchingFlights = true;
       }
     },
-    handleInputFrom: debounce(function() {
-      this.fetchLocations();
+    handleInputFrom: debounce(async function() {
+      this.filteredLocations = await this.fetchLocations(this.fromInput);
     }, 300),
-    handleInputDestination: debounce(function() {
-      this.fetchDestinations();
+    handleInputDestination: debounce(async function() {
+      this.filteredDestinations = await this.fetchLocations(this.destinationInput);
     }, 300),
     selectLocation(location, type) {
       if (type === 'from') {
@@ -224,10 +224,113 @@ export default {
         this.destinationInput = location.name;
         this.filteredDestinations = [];
       }
+    },
+    generateTravelersArray() {
+      try {
+        const travelers = [];
+        
+        // Vuxna (ADULT)
+        for (let i = 1; i <= this.adults; i++) {
+          travelers.push({
+            id: `ADT${i}`,
+            travelerType: 'ADULT',
+            count: 1 // Varje vuxen har en count på 1
+          });
+        }
+  
+        // Barn (CHILD)
+        for (let j = 1; j <= this.children; j++) {
+          travelers.push({
+            id: `CHD${j}`,
+            travelerType: 'CHILD',
+            count: 1 // Varje barn har en count på 1
+          });
+        }
+        
+        return travelers;
+      } catch (error) {
+        console.error('Fel vid generering av resenärer:', error);
+        this.errorFetchingFlights = true;
+        return [];
+      }
+    },
+    formatDate(dateTimeStr) {
+      // Funktion för att formatera datum och tid från ISO-format till läsbar sträng
+      const dateTime = new Date(dateTimeStr);
+      return dateTime.toLocaleString();
+    },
+    getNumberOfAdults(travelerPricings) {
+      let adultsCount = 0;
+      if (Array.isArray(travelerPricings)) {
+        travelerPricings.forEach((pricing) => {
+          if (pricing.travelerType === 'ADULT') {
+            adultsCount += pricing.travelerCount || 1; // Default till 1 om travelerCount inte finns
+          }
+        });
+      }
+      return adultsCount;
+    },
+    getNumberOfChildren(travelerPricings) {
+      let childrenCount = 0;
+      if (Array.isArray(travelerPricings)) {
+        travelerPricings.forEach((pricing) => {
+          if (pricing.travelerType === 'CHILD') {
+            childrenCount += pricing.travelerCount || 1; // Default till 1 om travelerCount inte finns
+          }
+        });
+      }
+      return childrenCount;
+    },
+    calculateWaitTime(firstArrival, nextDeparture) {
+    const arrivalTime = new Date(firstArrival);
+    const departureTime = new Date(nextDeparture);
+    const diffMs = departureTime - arrivalTime; // Skillnaden i millisekunder
+    const diffMinutes = Math.floor((diffMs / (1000 * 60)) % 60); // Extrahera antalet minuter
+    const diffHours = Math.floor((diffMs / (1000 * 60 * 60)) % 24); // Extrahera antalet timmar
+
+    if (diffHours > 0 && diffMinutes > 0) {
+      // Om både timmar och minuter är större än 0, visa både
+      return `${diffHours} timmar och ${diffMinutes} minuter`;
+    } else if (diffHours > 0 && diffMinutes === 0) {
+      // Om bara timmar är större än 0 och inga minuter, visa bara timmar
+      return `${diffHours} timmar`;
+    } else {
+      // Om timmar är 0 eller mindre, visa inget
+      return 'Mindre än en timme';
     }
+  },
+  
+//     getFlightNumber(result) {
+//   try {
+//     if (
+//       result && // Kontrollera om 'result' existerar och inte är null eller undefined
+//       result.itineraries && // Kontrollera om 'itineraries' egenskapen finns i 'result'
+//       result.itineraries.length > 0 && // Kontrollera om 'itineraries' arrayen har minst ett element
+//       result.itineraries[0].segments && // Kontrollera om 'segments' egenskapen finns i det första elementet av 'itineraries' arrayen
+//       result.itineraries[0].segments.length > 0 && // Kontrollera om 'segments' arrayen har minst ett segment
+//       result.itineraries[0].segments[0] && // Kontrollera om det första segmentet existerar
+//       result.itineraries[0].segments[0].carrierCode && // Kontrollera om 'carrierCode' egenskapen finns i det första segmentet
+//       result.itineraries[0].segments[0].number // Kontrollera om 'number' egenskapen finns i det första segmentet
+//     ) {
+//       return `${result.itineraries[0].segments[0].carrierCode}${result.itineraries[0].segments[0].number}`; // Returnera flygnumret som en sträng
+//     } else {
+//       console.warn('Ogiltigt resultatobjekt eller saknade egenskaper för flygnummer:', result); // Visa varning om något av kraven inte uppfylls
+//       return 'Flight Number N/A'; // Returnera ett standardvärde om kraven inte uppfylls
+//     }
+//   } catch (error) {
+//     console.error('Fel i getFlightNumber:', error); // Hantera eventuella fel som kan uppstå
+//     return 'Flight Number N/A'; // Returnera ett standardvärde vid fel
+//   }
+// }
+
+  
+   
+   
+    
   }
 };
 </script>
+
 
 <style scoped>
 .search-container {
@@ -291,6 +394,7 @@ h2 {
 
 .search-results {
   margin-top: 2rem;
+  color: white;
 }
 
 .search-results h3 {
@@ -302,3 +406,4 @@ h2 {
   margin-top: 1rem;
 }
 </style>
+
